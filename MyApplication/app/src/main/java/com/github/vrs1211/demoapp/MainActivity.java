360b193485868c3d2fe0e86f246a5d5fae254ff8
Vikas Ramji Singh <vrs1211@gmail.com>
2018-01-10 19:20:14 -0500

    initial push
    contains random experiments

---
+++ MyApplication/app/src/main/java/com/github/vrs1211/demoapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.github.vrs1211.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickDefault(View view)
+    {
+        Log.i("Click Event on: ",view.getTag().toString());
+
+    }
+}
